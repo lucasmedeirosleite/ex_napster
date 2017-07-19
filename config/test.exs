5588d810b7c283bed4746f63b659dd3332d5a963
Lucas Medeiros <lucastoc@gmail.com>
2017-07-18 21:15:32 -0300

    add configuration variables

---
+++ config/test.exs
@@ -0,0 +1,5 @@
+use Mix.Config
+
+config :ex_napster, api_base_url: "https://api.napster.com",
+                    api_version: "v2.2",
+                    api_key: "napster-api-key"
--- test/ex_napster_test.exs
+++ test/ex_napster_test.exs
@@ -1,8 +1,21 @@
 defmodule ExNapsterTest do
-  use ExUnit.Case
+  use ExUnit.Case, aync: true
   doctest ExNapster
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  describe "configuration" do
+    test "has an application env var for the base API Napster URL" do
+      napster_base_url = Application.get_env(:ex_napster, :api_base_url)
+      refute napster_base_url == nil
+    end
+
+    test "has an application env var for the Napster API version" do
+      napster_api_version = Application.get_env(:ex_napster, :api_version)
+      refute napster_api_version == nil
+    end
+
+    test "has an application env var for the Napster API Key" do
+      napster_api_key = Application.get_env(:ex_napster, :api_key)
+      refute napster_api_key == nil
+    end
   end
 end
