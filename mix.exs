3b7ed1d7f7ecaf35b14d31004826626a8f2be4cb
Lucas Medeiros <lucastoc@gmail.com>
2017-07-18 21:28:08 -0300

    update minimum elixir version

--- mix.exs
+++ mix.exs
@@ -9,7 +9,7 @@ defmodule ExNapster.Mixfile do
 
   def project do
     [app: :ex_napster,
-     elixir: ">= 1.2.0",
+     elixir: ">= 1.3.0",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps(),
