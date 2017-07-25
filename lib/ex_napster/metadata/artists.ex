a26ab6bf98e82c8bd133954af4746935f74b7101
Lucas Medeiros <lucastoc@gmail.com>
2017-07-25 11:22:19 -0300

    update docs

--- lib/ex_napster/metadata/artists.ex
+++ lib/ex_napster/metadata/artists.ex
@@ -1,4 +1,22 @@
 defmodule ExNapster.Metadata.Models.Artist do
+  @moduledoc """
+    Module to emulate an artist model
+  """
+
+  @doc """
+    Defines the Artist struct
+
+    * :id                 - the artist id
+    * :name               - the artist name
+    * :bios               - an array of artist biographies
+    * :blurb              - an array of artist blurb
+    * :shortcut           - the artist name shortcut
+    * :albums_ids         - the ids of the artist's albums
+    * :genres_ids         - the ids of the artist's genres
+    * :stations_ids       - the ids of the artist's stations
+    * :influences_ids     - the ids of the artist's influences
+    * :contemporaries_ids - the ids of the artist's contemporaries
+  """
   defstruct id: "",
             name: "",
             bios: [],
@@ -12,6 +30,18 @@ defmodule ExNapster.Metadata.Models.Artist do
 end
 
 defmodule ExNapster.Metadata.Models.Biography do
+  @moduledoc """
+    Module to emulate an artist biography
+  """
+
+  @doc """
+    Defines the Artist Biography struct
+
+    * :title              - the biography title
+    * :author             - the biography author
+    * :description        - the biography description
+    * :publish_date       - the biography publish date
+  """
   defstruct title: "", author: "", publish_date: nil, description: ""
 end
 
@@ -36,7 +66,7 @@ defmodule ExNapster.Metadata.Artists do
   - limit: It limits the total artists return (default: 20)
   - offset: Use for pagination (default: 0)
   - range: Defines the frequency of top artists, values are: week, month, year and life (default: month)
-  
+
   """
   def top(params \\ []) do
     with {:ok, response} <- Client.get(@top_artists, params),
